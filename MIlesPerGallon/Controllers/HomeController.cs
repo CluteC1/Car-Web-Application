ef3845670107debad881f32edf692cb00c5729ad
Christopher <christopher.m.clute@outlook.com>
2018-11-03 21:30:56 -0400

    Add project files.

---
+++ MIlesPerGallon/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MIlesPerGallon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome to my web application! Please enter your information under the Miles Per Gallon tab. This feature will allow you to calculate your vehicles miles per gallon!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Search()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MIlesPerGallon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MIlesPerGallon.MvcApplication" Language="C#" %>
